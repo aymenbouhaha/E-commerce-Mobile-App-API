bee3be7f226ebadb1f4206e7d1f7e4a3f9eb60ef
aymenbouhaha <92325496+aymenbouhaha@users.noreply.github.com>
2023-01-18 03:10:41 +0100

    authentication

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import {ConflictException, Injectable, NotFoundException} from '@nestjs/common';
+import {UserSignUpDto} from "./dto/usersignup.dto";
+import {InjectRepository} from "@nestjs/typeorm";
+import {UserEntity} from "./entity/user.entity";
+import {Repository} from "typeorm";
+import * as bcrypt from 'bcrypt';
+import {LoginDto} from "./dto/login.dto";
+import {JwtService} from "@nestjs/jwt";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @InjectRepository(UserEntity) private userRepository : Repository<UserEntity>,
+        private jwtService : JwtService
+    ) {
+    }
+
+    async signUp(userData: UserSignUpDto) : Promise<Partial<UserEntity>>{
+        const user = this.userRepository.create(
+            {
+                ...userData
+            }
+        )
+        user.salt = await bcrypt.genSalt();
+        user.password= await bcrypt.hash(user.password,user.salt)
+        try {
+            await this.userRepository.save(user);
+        }catch (e){
+            throw new ConflictException(`l'email et le mot de passe doivent etre unique`)
+        }
+        return {
+            id : user.id,
+            email : user.email,
+            username : user.username,
+            role : user.role
+        }
+    }
+
+
+    async login(credentials : LoginDto){
+        const {email , password} =credentials;
+        const user = await this.userRepository.findOneBy([{email : email}])
+
+        if (!user){
+            throw new NotFoundException(`l'email ou le mot de passe sont incorrecte`)
+        }
+        const hashedPassword = await bcrypt.hash(password, user.salt)
+        if (hashedPassword== user.password){
+            const payload = {
+                username : user.username,
+                email :user.email ,
+                role : user.role ,
+                image : user.image ,
+                phoneNumber : user.phoneNumber
+            }
+
+            const token = await this.jwtService.sign(payload)
+
+            return {
+                "token" : token
+            }
+
+        }else
+        {
+            throw new NotFoundException(`l'email ou le mot de passe sont incorrecte`)
+        }
+    }
+
+}
